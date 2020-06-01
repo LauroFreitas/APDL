bb7a588ed4d9b2ca9dbc674a9a981a275ee8a842
Lauro Henrique Santana de Freitas <lauroh.sfreitas@gmaill.com>
2020-06-01 15:22:38 -0300

    APDL-Project

---
+++ APDL-Versão Nova/Assets/Scripts/CutScene/CutScene1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene1 : MonoBehaviour
+{
+    public GameObject player;
+    public void Start()
+    {
+        StartCoroutine("Ativar");
+        
+    }
+    public IEnumerator Ativar()
+    {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        MetaEventSet.singleton.metaEvents[0].Event.Invoke();
+        yield return new WaitForSeconds(20);
+        gameObject.SetActive(false);
+        player.SetActive(true);
+    }
+}
---
+++ APDL-Versão Nova/Assets/Scripts/Player/Player1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class Player1 : MonoBehaviour
+{
+    private string RayCastTv = "TVDisable";//seleciona a tag Lanterna para o campo serial
+  
+    public Camera cam;//area para camera 
+    public GameObject TextView;
+   
+    private void Update()
+    {
+       RayCast();
+    }
+    private void RayCast() 
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 20f)) //serve para pegar a posição do alvo e para a lanterna tb
+        {
+            var selection = hit.transform;
+
+            if (selection.CompareTag(RayCastTv))
+            {
+                TextView.SetActive(true);
+                if (Input.GetKeyDown("e"))
+                {
+                    MetaEventSet.singleton.metaEvents[8].Event.Invoke();
+                    TextView.SetActive(false);
+                }
+            }
+            else 
+            {
+                TextView.SetActive(false);
+            }
+        }
+        
+    }
+}
